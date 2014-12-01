45b19fe70c0f354df8bb8da1045217921f455066
jkeston <john@kestwest.com>
2014-12-01 10:55:43 -0700

    Add the $local_path global variable

--- config_dropoff.php
+++ config_dropoff.php
@@ -1,11 +1,12 @@
 <?php
     // connect to database
-    $dbh = mysql_connect("localhost", "db_username", "db_password") or die("Connection failed");
-    mysql_select_db("unearthe_dropoff");
+    $dbh = mysql_connect("localhost", "your_db_user", "your_db_password") or die("Connection failed");
+    mysql_select_db("your_database");
 
     // Set timezine
     date_default_timezone_set('America/Chicago');
 
     $user_date = date('Y-m-d');
     $course_status = array('active','pending','archived');
+    $local_path = '/your_folder/';
 ?>
